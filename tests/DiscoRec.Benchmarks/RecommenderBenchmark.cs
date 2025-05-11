77487b89feef2dad123c3ad6173848b572793309
Andrew Kane <andrew@ankane.org>
2025-05-10 18:05:10 -0700

    Improved benchmarking [skip ci]

--- tests/DiscoRec.Benchmarks/RecommenderBenchmark.cs
+++ tests/DiscoRec.Benchmarks/RecommenderBenchmark.cs
@@ -17,5 +17,13 @@ public class RecommenderBenchmark
     public void Recommender()
     {
         var recommender = Recommender<int, string>.FitExplicit(data!, new RecommenderOptions { Factors = 20 });
+        foreach (var id in recommender.UserIds())
+        {
+            recommender.UserRecs(id, 5);
+        }
+        foreach (var id in recommender.ItemIds())
+        {
+            recommender.ItemRecs(id, 5);
+        }
     }
 }
